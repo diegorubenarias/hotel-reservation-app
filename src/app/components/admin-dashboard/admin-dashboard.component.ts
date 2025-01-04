6fb7b5cd90cff237e0ea80fabe05b105e8ad93e6
diego <diego.arias@gmail.com>
2025-01-04 12:12:52 -0300

    improve ui

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { AdminHeaderComponent } from '../admin-header/admin-header.component';
+import { AdminMenuComponent } from '../admin-menu/admin-menu.component';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [TableModule],
+  imports: [TableModule, AdminHeaderComponent, AdminMenuComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
--- src/app/components/admin-menu/admin-menu.component.ts
+++ src/app/components/admin-menu/admin-menu.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Menu } from 'primeng/menu';
+import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { Avatar } from 'primeng/avatar';
+import { AvatarGroup } from 'primeng/avatargroup';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-admin-menu',
   standalone: true,
-  imports: [],
+  imports: [Menu, BadgeModule, OverlayBadgeModule, Avatar, AvatarGroup],
   templateUrl: './admin-menu.component.html',
   styleUrl: './admin-menu.component.scss'
 })
-export class AdminMenuComponent {
+export class AdminMenuComponent implements OnInit{
+
+  items: MenuItem[] | undefined ;
+  ngOnInit() {
+    this.items = [
+        {
+            separator: true
+        },
+        {
+            label: 'Inicio',
+            items: [
+                {
+                    label: 'Tablero',
+                    icon: 'pi pi-home'
+                }
+            ]
+        },
+        {
+            label: 'Configuracion',
+            items: [
+                {
+                    label: 'Categorias',
+                    icon: 'pi pi-cog'
+                },
+                {
+                    label: 'Cerrar Sesion',
+                    icon: 'pi pi-sign-out',
+                    shortcut: '⌘+Q'
+                }
+            ]
+        },
+        {
+            separator: true
+        }
+    ];
+}
+
+  
 
 }
